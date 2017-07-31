4d15f276c79f382c2fa0441d8dfe6e06ab16fca1
James Taylor <james.jm.taylor@gmail.com>
2017-07-31 13:09:31 -0700

    Initial Commit

---
+++ mlSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mlSpike
+//
+//  Created by Taylor, James on 7/31/17.
+//  Copyright © 2017 Taylor, James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
